8b49fcf3e54ea0618fbd41dc5142a0a9ce16e3aa
Omri Ruvio <46563051+Omriruvio@users.noreply.github.com>
2023-07-14 05:42:26 +0000

    bar chart vs area connect nulls

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,57 @@
-import axios from 'axios';
-import './App.css';
-import { useEffect, useRef, useState } from 'react';
-import { Error } from './components/Error';
-import { forwardUrl } from './config';
-import _ from 'lodash';
+import { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Bar, BarChart } from 'recharts';
+import { data } from './data/mockdata';
 
+export default class Example extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/simple-area-chart-4ujxw';
 
-function App() {
-  const [title, setTitle] = useState<string>('');
-  const [data, setData] = useState<string[]>([]);
-  const [error, setError] = useState<string | string[]>('');
-  const [ignoredResponses, setIgnoredResponses] = useState<string[]>([]);
-  const DEBOUNCE_TIME = 1000;
-
-  useEffect(() => {
-    axios.get<string>(forwardUrl)
-      .then(res => setTitle(res.data))
-      .catch(err => {
-        console.log(err);
-        // setError(`Connection error. Possible CORS Error, set server port to public in codespace ports. Otherwise check the server is running on port 3333`)
-        setError(['Connection Error.', 'Possible CORS Error.', 'Set server port to public in codespace ports.', 'Otherwise check the server is running on port 3333.']);
-      })
-  }, []);
-
-  const currentRequest = useRef<string | null>(null);
-  const makeRequest = _.debounce(async () => {
-    const requestId = _.uniqueId();
-    currentRequest.current = requestId;
-    try {
-      const res = await axios.get<{ message: string }>(`${forwardUrl}/api/data`);
-      if (currentRequest.current === requestId) {
-        setData((data) => [...data, res.data.message]);
-      } else {
-        setIgnoredResponses((ignoredResponses) => [...ignoredResponses, res.data.message]);
-      }
-    }
-    catch (err) {
-      console.log(err);
-      setData((data) => [...data, 'Error']);
-    }
-  }, DEBOUNCE_TIME);
-
-  const handleClick = () => {
-    makeRequest();
-  };
-
-  return (
-    <div>
-      <Error text={error} />
-      <h1>{title}</h1>
-      <p style={{ color: 'aqua' }}>The button makes a {DEBOUNCE_TIME} ms debounced request, and only processes the last response.</p>
-      <p style={{ color: 'aqua' }}>Click the button with delays over {DEBOUNCE_TIME} ms to see the ignored responses.</p>
-      <p style={{ color: 'aqua' }}>Server processing time is set to 5 seconds.</p>
-      <button onClick={handleClick} style={{ marginTop: '50px' }}>Make Request</button>
-      <div style={{ position: 'fixed', bottom: 0, left: 0, padding: '1rem', border: '1px solid white', color: 'white', width: '33%', minHeight: '200px' }}>
-        <h2 style={{ marginTop: '2rem' }}>Processed Responses</h2>
-        {data.map((d, i) => <div key={i}>{d}</div>)}
+  render() {
+    return (
+      <>
+      <div style={{ width: '100vw', height: '200px' }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" tick={false} type="number" domain={[1689252216290, 1689252603440]}/>
+          <YAxis />
+          <Tooltip />
+          {/* <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" connectNulls={false}/> */}
+          <Bar dataKey="value" fill="#8884d8"/>
+        </BarChart>
+      </ResponsiveContainer>
       </div>
-      <div style={{ position: 'fixed', bottom: 0, right: 0, padding: '1rem', border: '1px solid white', color: 'white', width: '33%', minHeight: '200px' }}>
-        <h2>Ignored Responses</h2>
-        {ignoredResponses.map((d, i) => <div key={i} style={{ textDecoration: 'line-through' }}>{d}</div>)}
+      <div style={{ width: '100vw', height: '200px' }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" tick={false} type="number" domain={[1689252216290, 1689252603440]}/>
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" connectNulls={false}/>
+        </AreaChart>
+      </ResponsiveContainer>
       </div>
-
-    </div>
-  );
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/data/mockdata.ts
@@ -0,0 +1,366 @@
+export const data = [
+  {
+      "timestamp": 1689252216194,
+      "value": 0.18
+  },
+  {
+      "timestamp": 1689252216388,
+      "value": 0.34
+  },
+  {
+      "timestamp": 1689252216582,
+      "value": 0.57
+  },
+  {
+      "timestamp": 1689252216776,
+      "value": 0.35
+  },
+  {
+      "timestamp": 1689252216970,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252217164,
+      "value": 0.67
+  },
+  {
+      "timestamp": 1689252217358,
+      "value": 0.23
+  },
+  {
+      "timestamp": 1689252217552,
+      "value": 0.36
+  },
+  {
+      "timestamp": 1689252217746,
+      "value": 0.42
+  },
+  {
+      "timestamp": 1689252217940,
+      "value": 0.74
+  },
+  {
+      "timestamp": 1689252218134,
+      "value": 0.35
+  },
+  {
+      "timestamp": 1689252218328,
+      "value": 0.54
+  },
+  {
+      "timestamp": 1689252218522,
+      "value": 0.44
+  },
+  {
+      "timestamp": 1689252218716,
+      "value": 0.32
+  },
+  {
+      "timestamp": 1689252218910,
+      "value": 0.35
+  },
+  {
+      "timestamp": 1689252219104,
+      "value": 0.35
+  },
+  {
+      "timestamp": 1689252219298,
+      "value": 0.64
+  },
+  {
+      "timestamp": 1689252219492,
+      "value": 0.27
+  },
+  {
+      "timestamp": 1689252219686,
+      "value": 0.43
+  },
+  {
+      "timestamp": 1689252219880,
+      "value": 0.3
+  },
+  {
+      "timestamp": 1689252220074,
+      "value": 0.53
+  },
+  {
+      "timestamp": 1689252220268,
+      "value": 0.32
+  },
+  {
+      "timestamp": 1689252220462,
+      "value": 0.51
+  },
+  {
+      "timestamp": 1689252220656,
+      "value": 0.71
+  },
+  {
+      "timestamp": 1689252220850,
+      "value": 0.5
+  },
+  {
+      "timestamp": 1689252221044,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252221238,
+      "value": 0.5
+  },
+  {
+      "timestamp": 1689252221432,
+      "value": 0.37
+  },
+  {
+      "timestamp": 1689252221626,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252221820,
+      "value": 0.34
+  },
+  {
+      "timestamp": 1689252222014,
+      "value": 0.41
+  },
+  {
+      "timestamp": 1689252222208,
+      "value": 0.42
+  },
+  {
+      "timestamp": 1689252222402,
+      "value": 0.61
+  },
+  {
+      "timestamp": 1689252222596,
+      "value": 0.59
+  },
+  {
+      "timestamp": 1689252222790,
+      "value": 0.46
+  },
+  {
+      "timestamp": 1689252222984,
+      "value": 0.39
+  },
+  {
+      "timestamp": 1689252223178,
+      "value": 0.57
+  },
+  {
+      "timestamp": 1689252223372,
+      "value": 0.25
+  },
+  {
+      "timestamp": 1689252223566,
+      "value": 0.57
+  },
+  {
+      "timestamp": 1689252223760,
+      "value": 0.27
+  },
+  {
+      "timestamp": 1689252223954,
+      "value": 0.32
+  },
+  {
+      "timestamp": 1689252224148,
+      "value": 0.69
+  },
+  {
+      "timestamp": 1689252224342,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252224536,
+      "value": 0.39
+  },
+  {
+      "timestamp": 1689252224730,
+      "value": 0.19
+  },
+  {
+      "timestamp": 1689252224924,
+      "value": 0.42
+  },
+  {
+      "timestamp": 1689252225118,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252225312,
+      "value": 0.46
+  },
+  {
+      "timestamp": 1689252225506,
+      "value": 0.5
+  },
+  {
+      "timestamp": 1689252225700,
+      "value": 0.5
+  },
+  {
+      "timestamp": 1689252225894,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252226088,
+      "value": 0.28
+  },
+  {
+      "timestamp": 1689252226282,
+      "value": 0.46
+  },
+  {
+      "timestamp": 1689252226476,
+      "value": 0.21
+  },
+  {
+      "timestamp": 1689252226670,
+      "value": 0.23
+  },
+  {
+      "timestamp": 1689252226864,
+      "value": 0.46
+  },
+  {
+      "timestamp": 1689252596822,
+      "value": 2.19
+  },
+  {
+      "timestamp": 1689252597016,
+      "value": 7.36
+  },
+  {
+      "timestamp": 1689252597210,
+      "value": 8.42
+  },
+  {
+      "timestamp": 1689252597404,
+      "value": 7.22
+  },
+  {
+      "timestamp": 1689252597598,
+      "value": 7.59
+  },
+  {
+      "timestamp": 1689252597792,
+      "value": 9.02
+  },
+  {
+      "timestamp": 1689252597986,
+      "value": 8.49
+  },
+  {
+      "timestamp": 1689252598180,
+      "value": 7.93
+  },
+  {
+      "timestamp": 1689252598374,
+      "value": 7.36
+  },
+  {
+      "timestamp": 1689252598568,
+      "value": 7.13
+  },
+  {
+      "timestamp": 1689252598762,
+      "value": 8.23
+  },
+  {
+      "timestamp": 1689252598956,
+      "value": 7.43
+  },
+  {
+      "timestamp": 1689252599150,
+      "value": 9.02
+  },
+  {
+      "timestamp": 1689252599344,
+      "value": 7.43
+  },
+  {
+      "timestamp": 1689252599538,
+      "value": 6.9
+  },
+  {
+      "timestamp": 1689252599732,
+      "value": 5.98
+  },
+  {
+      "timestamp": 1689252599926,
+      "value": 6.67
+  },
+  {
+      "timestamp": 1689252600120,
+      "value": 6.21
+  },
+  {
+      "timestamp": 1689252600314,
+      "value": 5.1
+  },
+  {
+      "timestamp": 1689252600508,
+      "value": 8.74
+  },
+  {
+      "timestamp": 1689252600702,
+      "value": 7.64
+  },
+  {
+      "timestamp": 1689252600896,
+      "value": 5.75
+  },
+  {
+      "timestamp": 1689252601090,
+      "value": 5.98
+  },
+  {
+      "timestamp": 1689252601284,
+      "value": 5.98
+  },
+  {
+      "timestamp": 1689252601478,
+      "value": 5.52
+  },
+  {
+      "timestamp": 1689252601672,
+      "value": 5.52
+  },
+  {
+      "timestamp": 1689252601866,
+      "value": 5.7
+  },
+  {
+      "timestamp": 1689252602060,
+      "value": 5.45
+  },
+  {
+      "timestamp": 1689252602254,
+      "value": 4.46
+  },
+  {
+      "timestamp": 1689252602448,
+      "value": 5.06
+  },
+  {
+      "timestamp": 1689252602642,
+      "value": 5.57
+  },
+  {
+      "timestamp": 1689252602836,
+      "value": 5.66
+  },
+  {
+      "timestamp": 1689252603030,
+      "value": 5.38
+  },
+  {
+      "timestamp": 1689252603224,
+      "value": 4.03
+  },
+  {
+      "timestamp": 1689252603418,
+      "value": 3.7
+  }
+]
